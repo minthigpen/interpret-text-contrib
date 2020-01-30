ae7165dde2d21d64eb4edcd458541b8f98dbf13a
Shannon Hwang <v-yohwa@microsoft.com>
2020-01-30 15:17:08 -0500

    adding back needed params and first draft of integration test

--- test/conftest.py
+++ test/conftest.py
@@ -7,6 +7,7 @@ def path_notebooks():
     """Returns the path of the notebooks folder"""
     return os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, "notebooks"))
 
+
 @pytest.fixture(scope="module")
 def notebooks():
     folder_notebooks = path_notebooks()
@@ -14,9 +15,13 @@ def notebooks():
     # Path for the notebooks
     paths = {
         "tc_mnli_bert": os.path.join(folder_notebooks, "text_classification", "text_classification_mnli_bert.ipynb"),
-        "text_classification_mnli_bow_lr": os.path.join(folder_notebooks, "text_classification", "text_classification_mnli_bow_lr.ipynb")
+        "text_classification_mnli_bow_lr": os.path.join(folder_notebooks, "text_classification",
+                                                        "text_classification_mnli_bow_lr.ipynb"),
+        "text_classification_sst2_three_player": os.path.join(folder_notebooks, "text_classification",
+                                                              "text_classification_sst2_three_player.ipynb")
     }
     return paths
+
 
 @pytest.fixture
 def tmp(tmp_path_factory):
